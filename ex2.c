b68104df0b9d5eea408e329b147fb0507833ea46
yuvalmor <yuvalmor94@gmail.com>
2019-03-25 20:29:40 +0200

    25.03 update

--- ex2.c
+++ ex2.c
@@ -1,5 +1,6 @@
-//  Yuval Mor - 205380173
+// Yuval Mor - 205380173
 
+#include <sys/wait.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
@@ -8,14 +9,29 @@
 #include <malloc.h>
 #include <stdlib.h>
 
-#define MAX_NUM_JOBS 250
 #define MAX_CHAR_PER_LINE 250
+#define MAX_JOBS 512
 #define PROMPT ">"
 #define SPACE " "
+#define BACKGROUND "&"
+#define SYSTEM_CALL_FAILURE -1
+#define FAILURE -1
+#define FIRST_PLACE 0
+#define CHILDREN_PID 0
+#define IDENTICAL 0
+#define EMPTY 0
+#define NOT_EMPTY 1
+#define FORK_ERROR "fork error"
+#define EXECVP_ERROR "execvp error"
+#define WAIT_ERROR "wait error"
+#define SCAN_WITHOUT_SPACES "%[^\n]"
+#define ADD_CHAR(x) (x+1)
+#define LAST_PLACE(x) (x-1)
 
 typedef struct{
     pid_t pid;
-    char* command;
+    char** command;
+    int commandSize;
 }Job;
 
 char** getToken(char* command, int* size){
@@ -25,7 +41,7 @@ char** getToken(char* command, int* size){
     while (token != NULL){
         args = realloc(args,(i+1)* sizeof(char*));
         if (args == NULL){
-            exit(-1);
+            exit(EXIT_FAILURE);
         }
         args[i] = token;
         token = strtok(NULL,SPACE);
@@ -35,33 +51,97 @@ char** getToken(char* command, int* size){
     return args;
 }
 
-void freeJob(Job* job){
-    free(job->command);
-}
-
-void printJob(Job* job){
-    printf("%d %s",job->pid,job->command);
-}
-
 char* getCommand(){
     char* command;
-    char line [MAX_CHAR_PER_LINE];
-    scanf("%[^\n]",line);
-    command = (char*)malloc(strlen(line)+1);
-    strcpy(command,line);
+    char buffer [MAX_CHAR_PER_LINE];
+    scanf(SCAN_WITHOUT_SPACES,buffer);
+    // Allocate the length of the command plus one char for the \0
+    command = (char*)malloc(ADD_CHAR(strlen(buffer)));
+    if(command==NULL){
+        exit(EXIT_FAILURE);
+    }
+    strcpy(command,buffer);
     return command;
 }
 
+int findFreePlace(int jobsPlaces[MAX_JOBS]){
+    int i = 0;
+    while (i < MAX_JOBS){
+        if(jobsPlaces[i] == EMPTY){
+            return i;
+        }
+        i++;
+    }
+    return FAILURE;
+}
+
+Job* newJob(pid_t pid,char** command, int commandSize){
+    Job* job = (Job*)malloc(sizeof(Job));
+    job->pid=pid;
+    job->command = command;
+    job->commandSize = commandSize;
+    return job;
+}
+
+void addJob(Job* jobs[MAX_JOBS],int jobsPlaces[MAX_JOBS],pid_t pid,char** command, int commandSize){
+    int place = findFreePlace(jobsPlaces);
+    jobsPlaces[place] = NOT_EMPTY;
+    jobs[place] = newJob(pid,command,commandSize);
+}
+
+void freeJob(Job* jobToFree){
+    for(int i=0; i<jobToFree->commandSize;i++){
+        free(jobToFree->command[i]);
+        free(jobToFree->command);
+        free(jobToFree);
+    }
+}
+
+void deleteJob(Job* jobs[MAX_JOBS],int jobsPlaces[MAX_JOBS],pid_t pid){
+    bool flag = true;
+    int i = 0;
+    while (flag){
+        if(jobs[i]->pid == pid){
+            flag = false;
+            free(jobs[i]);
+            jobsPlaces[i] = 0;
+        }
+    }
+}
+
+void preformJob(char** args,Job* jobs[MAX_JOBS],int jobsPlaces[MAX_JOBS],int commandSize){
+    pid_t pid;
+    if((pid = fork()) == SYSTEM_CALL_FAILURE){
+        perror(FORK_ERROR);
+    }
+    if(pid == CHILDREN_PID){
+        printf("%d\n",getpid());
+        execvp(args[FIRST_PLACE],args);
+        perror(EXECVP_ERROR);
+    }
+    addJob(jobs,jobsPlaces,pid,args,commandSize);
+    if(waitpid(pid,NULL,0) == SYSTEM_CALL_FAILURE){
+        perror(WAIT_ERROR);
+    }
+}
+
 int main() {
-    int size;
-    char** args =NULL;
-    bool keepRunning = true;
+    int jobsPlaces[MAX_JOBS]={0};
+    Job* jobs[MAX_JOBS];
+    int commandSize;
+    char** args;
     char* command;
+    bool keepRunning = true;
     while (keepRunning){
         printf(PROMPT);
         command = getCommand();
-        args = getToken(command,&size);
-        keepRunning=false;
+        args = getToken(command,&commandSize);
+        if(strcmp(args[LAST_PLACE(commandSize)],BACKGROUND) == IDENTICAL){
+
+        } else {
+           preformJob(args,jobs,jobsPlaces,commandSize);
+        }
+        keepRunning = false;
     }
     return 0;
 }
